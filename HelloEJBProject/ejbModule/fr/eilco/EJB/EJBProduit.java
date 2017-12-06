0ca3eaa1ca6e1fc6da56663c054a2ef2509011cd
Alfaty <alban.lermechain@sfr.fr>
2017-12-06 11:21:27 +0100

    <fix>

--- HelloEJBProject/ejbModule/fr/eilco/EJB/EJBProduit.java
+++ HelloEJBProject/ejbModule/fr/eilco/EJB/EJBProduit.java
@@ -3,10 +3,6 @@ package fr.eilco.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-<<<<<<< HEAD
-=======
-
->>>>>>> 34ebca0c674a5c3a1d3e5af28c607a60e4e31612
 import fr.eilco.facade.facadeProduit;
 import fr.eilco.model.Produit;
 /**
@@ -28,12 +24,6 @@ public class EJBProduit implements EJBProduitRemote, EJBProduitLocal {
     }
 
 
-<<<<<<< HEAD
-
-
-	@Override
-=======
->>>>>>> 34ebca0c674a5c3a1d3e5af28c607a60e4e31612
 	public Produit getProduit(int ID) {
 	   facadeProduit fp= new facadeProduit();
 	   return fp.getProduit(ID, mh);
