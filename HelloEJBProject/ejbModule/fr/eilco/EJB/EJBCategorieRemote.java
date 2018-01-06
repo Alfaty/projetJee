9169001ca960a29ec0619e9c9599bf58e6963228
Alfaty <alban.lermechain@sfr.fr>
2018-01-06 16:41:53 +0100

    s

--- HelloEJBProject/ejbModule/fr/eilco/EJB/EJBCategorieRemote.java
+++ HelloEJBProject/ejbModule/fr/eilco/EJB/EJBCategorieRemote.java
@@ -1,11 +1,12 @@
 package fr.eilco.EJB;
 
 import javax.ejb.Remote;
+
 import fr.eilco.model.Categorie;
 
-
+@Remote
 public interface EJBCategorieRemote {
 
-	
 	public Categorie getCategorie(int ID);
+		
 }
